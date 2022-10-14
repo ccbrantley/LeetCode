e6aa83519a35151514728c0ae904b77d54fc7105
ccbrantley <chrisbrantley1993@gmail.com>
2022-10-14 14:48:23 -0400

    Create AddBinary.py

---
+++ AddBinary/AddBinary.py
@@ -0,0 +1,73 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        c = []
+        indexA = len(a) - 1
+        indexB = len(b) - 1
+        carry = 0
+        while indexA >= 0 or indexB >= 0:
+            while indexA >= 0 and indexB >= 0:
+                valueA = a[indexA]
+                valueB = b[indexB]
+                print("A: {}, B: {}".format(valueA, valueB))
+                if (valueA == "1" and valueB == "1" ):
+                    print("here 1")
+                    if (carry):
+                        c.insert(0, "1")
+                    else:
+                        c.insert(0, "0")
+                    carry = 1
+                elif (valueA == "1"  or valueB == "1" ):
+                    print("here 2")
+                    if (carry):
+                        c.insert(0, "0")
+                        carry = 1
+                    else:
+                        c.insert(0, "1")
+                else:
+                    print("here 3")
+                    if (carry):
+                        c.insert(0, "1")
+                        carry = 0
+                    else:
+                        c.insert(0, "0")  
+                indexA -= 1
+                indexB -= 1
+            while indexA >= 0:
+                valueA = a[indexA]
+                if (valueA == "1" ):
+                    if (carry):
+                        c.insert(0, "0")
+                        carry = 1
+                    else:
+                        c.insert(0, "1")
+                else:
+                    if (carry):
+                        c.insert(0, "1")
+                        carry = 0
+                    else:
+                        c.insert(0, "0")
+                indexA -= 1
+            while indexB >= 0:
+                valueB = b[indexB]
+                if (valueB == "1" ):
+                    if (carry):
+                        c.insert(0, "0")
+                        carry = 1
+                    else:
+                        c.insert(0, "1")
+                else:
+                    if (carry):
+                        c.insert(0, "1")
+                        carry = 0
+                    else:
+                        c.insert(0, "0")
+                indexB -= 1
+            if (carry):
+                c.insert(0, "1")
+                carry = 0
+        return ''.join(c)
