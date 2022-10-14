d2efe11c90546b92550c8d25b16cadbb4e9ae0da
ccbrantley <chrisbrantley1993@gmail.com>
2022-10-14 14:08:59 -0400

    Update TwoSum.py

--- TwoSum/TwoSum.py
+++ TwoSum/TwoSum.py
@@ -1,1 +1,27 @@
+class Solution(object):
+    """
+    :type nums: List[int] -> array of integers
+    :type target: int
+    :rtype: List[int]
+    """
+    def linearSort(nums):
+        curValue = nums[0]
+        change = True
+        while (change):
+            change = False
+            for index0 in range(len(nums) - 1):
+                if (nums[index0] > nums[index0 + 1]):
+                    temp = nums[index0]
+                    nums[index0] = nums[index0 + 1]
+                    nums[index0 + 1] = temp
+                    change = True
 
+    def twoSum(self, nums, target):
+        #[0, 3, 3, 5]
+        solution = []
+        for index0 in range(len(nums)):
+            for index1 in range(index0 + 1, len(nums)):
+                if ((nums[index0] + nums[index1]) == target):
+                    solution.append(index0)
+                    solution.append(index1)
+        return solution
