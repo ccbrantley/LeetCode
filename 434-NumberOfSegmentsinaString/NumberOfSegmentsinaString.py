5e39666d7545c7953ef7e9aa7f8092a155ee4a49
ccbrantley <chrisbrantley1993@gmail.com>
2023-02-07 14:38:00 -0500

    Update NumberOfSegmentsinaString.py

--- 434-NumberOfSegmentsinaString/NumberOfSegmentsinaString.py
+++ 434-NumberOfSegmentsinaString/NumberOfSegmentsinaString.py
@@ -1,1 +1,11 @@
-
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        segCount = 0
+        for x in s.split(" "):
+            if x != "":
+                segCount += 1
+        return segCount
