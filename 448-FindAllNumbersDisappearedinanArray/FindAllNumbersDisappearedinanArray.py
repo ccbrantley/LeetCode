97f5edbb72ac3c2638ddaa0fe512e456bd8afa37
ccbrantley <chrisbrantley1993@gmail.com>
2023-02-08 11:40:35 -0500

    Update FindAllNumbersDisappearedinanArray.py

--- 448-FindAllNumbersDisappearedinanArray/FindAllNumbersDisappearedinanArray.py
+++ 448-FindAllNumbersDisappearedinanArray/FindAllNumbersDisappearedinanArray.py
@@ -1,1 +1,13 @@
-
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        seenDict = {x : True for x in nums}
+        unseenList = []
+        for x in range(1, len(nums) + 1):
+            value = seenDict.setdefault(x, False)
+            if (not value):
+                unseenList.append(x)
+        return unseenList
