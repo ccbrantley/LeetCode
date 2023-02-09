245a73fd4c6b051924725b7118177d48301b2822
ccbrantley <chrisbrantley1993@gmail.com>
2023-02-09 14:24:36 -0500

    Update HammingDistance.py

--- 461-HammingDistance/HammingDistance.py
+++ 461-HammingDistance/HammingDistance.py
@@ -1,1 +1,16 @@
-
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        bX = bin(x)[2:]
+        bY = bin(y)[2:]
+        bX = "0" * (len(bY) - len(bX)) + bX
+        bY = "0" * (len(bX) - len(bY)) + bY
+        dCount = 0
+        for idx in range(len(bX)):
+            if bX[idx] != bY[idx]:
+                dCount += 1
+        return dCount
