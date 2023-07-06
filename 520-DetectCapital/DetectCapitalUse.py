07bd6fdba6d658c6b140fc5d775be613193f1615
ccbrantley <chrisbrantley1993@gmail.com>
2023-07-06 16:03:17 -0400

    Update DetectCapitalUse.py

--- 520-DetectCapital/DetectCapitalUse.py
+++ 520-DetectCapital/DetectCapitalUse.py
@@ -1,1 +1,19 @@
-
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        nC = 0
+        if len(word) == 1:
+            return True
+        for letter in word:
+            if letter.isupper():
+                nC += 1
+        if nC < len(word):
+            if nC == 1 and word[0].isupper():
+                return True
+            elif nC == 0:
+                return True
+        elif nC == len(word):
+            return True
